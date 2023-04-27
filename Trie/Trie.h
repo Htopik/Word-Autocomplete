45f3562e13538c74e5fee539403da9d7c94d3e53
Htopik <49284279+Htopik@users.noreply.github.com>
2023-04-27 16:27:43 +0400

    Мелкие правки

--- Trie/Trie.h
+++ Trie/Trie.h
@@ -6,7 +6,6 @@ const int ALPHABET_SIZE = 26;
 struct TrieNode
 {
 	struct TrieNode* children[ALPHABET_SIZE];
-	// isEndOfWord - true, если ключ является концом слова
 	bool isEndOfWord;
 };
 
@@ -15,6 +14,5 @@ void insert(TrieNode*, string);
 bool search(TrieNode*, string);
 bool isEmpty(TrieNode*);
 TrieNode* remove(TrieNode*, string, int depth = 0);
-void findMinPrefixes(TrieNode*, char[], int, string&);
 vector<string> findWords(TrieNode*, string);
 
